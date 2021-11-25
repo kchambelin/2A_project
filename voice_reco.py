f33577c91de9e89afaed813bb8f110054a14cfba
kchambelin <kevin.chambelin@sigma-clermont.fr>
2021-11-25 11:18:10 +0100

    debug_voc_reco

--- voice_reco.py
+++ voice_reco.py
@@ -1,22 +1,37 @@
-import speech_recognition
-import pyttsx3
+# import speech_recognition
+# import pyttsx3
 
-recognizer=speech_recognition.Recognizer()
+# recognizer=speech_recognition.Recognizer()
 
-while True:
+# while True:
 
-    try:
-        with speech_recognition.Microphone() as mic:
+#     try:
+#         with speech_recognition.Microphone() as mic:
+#             print('test')
 
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio=recognizer.listen(mic)
+#             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+#             audio=recognizer.listen(mic)
 
-            text=recognizer.recognize_google(audio)
-            text=text.lower()
+#             text=recognizer.recognize_google(audio)
+#             text=text.lower()
 
-            print(text)
+#             print(text)
     
-    except speech_recognition.UnknownValueError():
+#     except :
 
-        recognizer=speech_recognition.Recognizer()
-        continue
+#         recognizer=speech_recognition.Recognizer()
+#         continue
+
+import speech_recognition as sr
+rObject = sr.Recognizer() 
+audio = ''
+with sr.Microphone(device_index=0) as source: 
+    print("Speak...") 
+    rObject.adjust_for_ambient_noise(source, duration=5)  
+    audio = rObject.listen(source, phrase_time_limit = 0) 
+    print("Stop.")
+    try: 
+        text = rObject.recognize_google(audio, language ='fr-FR') 
+        print("You : "+ text)  
+    except: 
+        print("Could not understand your audio...PLease try again !") 
