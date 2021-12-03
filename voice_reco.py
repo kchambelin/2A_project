dce3e2a7bcaa170bfda6787df8d5b2d0d636623f
batguerin <bguerin4@gmail.com>
2021-12-03 14:28:40 +0100

    code_avec_reco_de_mot

--- voice_reco.py
+++ voice_reco.py
@@ -5,11 +5,17 @@ audio = ''
 
 with sr.Microphone(device_index=0) as source: 
     print("Speak...") 
-    rObject.adjust_for_ambient_noise(source, duration=5)  
+    rObject.adjust_for_ambient_noise(source, duration=2)  
     audio = rObject.listen(source, phrase_time_limit = 0) 
     print("Stop.")
     try: 
         text = rObject.recognize_google(audio) 
         print("You : "+ text)  
+        # reco des mots
+        if text=="hello":
+            print("yes")
+
+            
     except: 
-        print("Could not understand your audio...PLease try again !")  
+        print("Could not understand your audio...PLease try again !")
+
