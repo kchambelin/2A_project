0f90bc30e3db4425dcb5ebeb1ed37dd21f244b19
batguerin <bguerin4@gmail.com>
2021-12-03 14:36:22 +0100

    test

--- voice_reco.py
+++ voice_reco.py
@@ -5,15 +5,19 @@ audio = ''
 
 with sr.Microphone(device_index=0) as source: 
     print("Speak...") 
-    rObject.adjust_for_ambient_noise(source, duration=2)  
+    rObject.adjust_for_ambient_noise(source, duration=1)  
     audio = rObject.listen(source, phrase_time_limit = 0) 
     print("Stop.")
     try: 
         text = rObject.recognize_google(audio) 
         print("You : "+ text)  
         # reco des mots
-        if text=="hello":
-            print("yes")
+        if text=="start":
+            print("start the robot")
+        elif text=="order":
+            print("robot do the order")
+        elif text=="stop":
+            print("stop the robot")
 
             
     except: 
