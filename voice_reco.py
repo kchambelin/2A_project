63c854dc6c0cc9f8866a5feaee2ab56d5bc99068
batguerin <bguerin4@gmail.com>
2021-12-03 16:34:43 +0100

    doesn_t_work

--- voice_reco.py
+++ voice_reco.py
@@ -1,25 +1,34 @@
 import speech_recognition as sr
 
-rObject = sr.Recognizer() 
-audio = ''
+def voice_reco():
+    rObject = sr.Recognizer() 
+    audio = ''
 
-with sr.Microphone(device_index=0) as source: 
-    print("Speak...") 
-    rObject.adjust_for_ambient_noise(source, duration=1)  
-    audio = rObject.listen(source, phrase_time_limit = 0) 
-    print("Stop.")
-    try: 
-        text = rObject.recognize_google(audio) 
-        print("You : "+ text)  
-        # reco des mots
-        if text=="start":
-            print("start the robot")
-        elif text=="order":
-            print("robot do the order")
-        elif text=="stop":
-            print("stop the robot")
+    with sr.Microphone(device_index=0) as source: 
+        print("Speak...") 
+        rObject.adjust_for_ambient_noise(source, duration=2)  
+        audio = rObject.listen(source, phrase_time_limit = 0) 
+        rObject.pause_threshold = 0.8 
+        print("Stop.")
+        try: 
+            text = rObject.recognize_google(audio) 
+            print("You : "+ text)  
+            return text
 
+        except: 
+            print("Could not understand your audio...PLease try again !")
+            return
             
-    except: 
-        print("Could not understand your audio...PLease try again !")
 
+while True:
+
+    if voice_reco()=="start":
+       print("start the robot")
+
+    elif voice_reco()=="order":                
+        print("robot do the order")
+
+    elif voice_reco()=="stop":
+        break  
+    else :
+        print("anything")  
