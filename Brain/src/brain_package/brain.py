6c5df0ab5c089b07cd4f623b0356838025c64b34
kchambelin <kevin.chambelin@sigma-clermont.fr>
2022-02-15 10:21:06 +0100

    start_function

--- Brain/src/brain_package/brain.py
+++ Brain/src/brain_package/brain.py
@@ -1,3 +1,7 @@
+def call_robot_home_conf() :
+    # send 0 to robot
+    return 1
+
 def initialize() :
     Orders = [
         [0, "stop"],
@@ -6,6 +10,8 @@ def initialize() :
         [3, "check"],
         [4, "pick"]
     ]
+
+    call_robot_home_conf()
 
 def ask_voc_reco_for_order() :
 
@@ -31,7 +37,23 @@ def verify_pos_cam(piece) :
         piece = 0
     return piece
     
-def compute_real_coordinates
+def compute_real_coordinates(x_cam, y_cam, z_cam) :
+    coordinates = [x_cam, y_cam, z_cam]
+    return coordinates
+
+def compute_destination_coordinates(coordinates) :
+
+    return coordinates
+
+def call_robot_to_grab(coordinates) :
+
+    response = 0 # -1 if not grabed, 0 else
+    return response
+
+
+def call_robot_to_release(coordinates) :
+    response = 0 
+
 
 def start() :
     pieces = get_all_pieces_from_cam()
@@ -40,6 +62,22 @@ def start() :
         if piece == 0 :
             continue
         piece_coordinates = compute_real_coordinates(piece[2], piece[3], piece[4])
+        piece_destination_coordinates = compute_destination_coordinates(piece_coordinates)
+        piece_grabed = -1
+        while piece_grabed == -1 :
+            piece_grabed = call_robot_to_grab(piece_coordinates)
+        call_robot_to_release(piece_destination_coordinates)
+
+def test() :
+    return 'something'
+
+def check() :
+    return 'something else'
+
+def pick() :
+    return 'something else again'
+
+
 
 initialize()
 while True :
