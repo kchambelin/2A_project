2bf7f8528d72b1b06ac213826af0cc8000b66221
Baptiste <bguerin4@gmail.com>
2022-03-25 14:28:41 +0100

    working_code

--- voice_reco.py
+++ voice_reco.py
@@ -1,4 +1,8 @@
 import speech_recognition as sr
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+
 
 def voice_reco():
     rObject = sr.Recognizer() 
@@ -6,7 +10,7 @@ def voice_reco():
 
     with sr.Microphone(device_index=0) as source: 
         print("Speak...") 
-        rObject.adjust_for_ambient_noise(source, duration=2)  
+        rObject.adjust_for_ambient_noise(source, duration=0.5)  
         audio = rObject.listen(source, phrase_time_limit = 0) 
         rObject.pause_threshold = 0.8 
         print("Stop.")
@@ -21,14 +25,31 @@ def voice_reco():
             
 
 while True:
+    
+    text = voice_reco()
+    if text!=None:
+        text_token = word_tokenize(text)
+        print(text_token)
+        text_token = re.sub(r"[^a-zA-Z0-9]", " ", str(text.lower()))
+        text_token_lower = text_token.split()
+        print(text_token_lower)
+        filtered_list = []
+        spe_words = ["begin", "stop", "blue", "red", "green", "square", "triangle", "cylinder", "star"]
+        for word in text_token_lower:
+            if word.casefold() in spe_words:
+                filtered_list.append(word)
+        print(filtered_list)
+        
+        if filtered_list!=[]:
 
-    if voice_reco()=="start":
-       print("start the robot")
+            if "begin" in filtered_list:
+                print("start the robot")
 
-    elif voice_reco()=="order":                
-        print("robot do the order")
+            elif "reproduce" in filtered_list:                
+                print("robot do the order")
+                
+            elif "blue" and "star" in filtered_list:                
+                print("robot do the order")
 
-    elif voice_reco()=="stop":
-        break  
-    else :
-        print("anything")
+            elif "stop" in filtered_list:
+                break
